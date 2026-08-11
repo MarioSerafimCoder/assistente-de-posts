c95527f81c048805aee33afe12e341e6d6490719
MarioSerafimCoder <185074209+MarioSerafimCoder@users.noreply.github.com>
2026-08-11 16:34:17 -0300

    Alinha elemento do Feed à logo Verti

--- src/lib/__tests__/verti-visual.test.ts
+++ src/lib/__tests__/verti-visual.test.ts
@@ -61,7 +61,7 @@ describe("aparência da Verti · Opção 01", () => {
     expect(feedHtml).toMatch(/\.verti-callout\{[^}]*font-weight:400/);
     expect(feedHtml).toMatch(/\.verti-callout\{[^}]*line-height:1\.35/);
     expect(feedHtml).toMatch(/\.verti-callout-text\{[^}]*text-align:left/);
-    expect(feedHtml).toMatch(/\.verti-outline\{[^}]*bottom:24px/);
+    expect(feedHtml).toMatch(/\.verti-outline\{[^}]*bottom:-310px/);
     expect(storyHtml).toMatch(/\.verti-headline\{[^}]*font-size:80px/);
     expect(storyHtml).toMatch(/\.verti-headline\{[^}]*top:238px/);
 
